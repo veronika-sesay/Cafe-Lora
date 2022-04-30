ccb398cc885c6e250ad1a7a5378d832289ca7376
Veronika Sesay <veronika.sesay@gmail.com>
2022-04-30 12:30:20 +0200

    JS pro objednávání

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css';
 
 console.log('funguju!');
 
+//skrývání menu
+
 const navBtn = document.getElementById('nav-btn');
 const nav = document.querySelector('nav');
 navBtn.addEventListener('click', () => {
@@ -14,3 +16,21 @@ navAll.forEach((item) =>
     nav.classList.add('nav-closed');
   }),
 );
+
+//objednávání kávy
+
+const orderBtn = document.querySelector('.order-btn');
+const drinkCup = document.querySelector('.drink__cup');
+
+let ordered = false;
+orderBtn.addEventListener('click', () => {
+  if (ordered === false) {
+    orderBtn.textContent = 'Zrušit';
+    drinkCup.classList.add('drink__cup--selected');
+    ordered = true;
+  } else {
+    orderBtn.textContent = 'Objednat';
+    drinkCup.classList.remove('drink__cup--selected');
+    ordered = false;
+  }
+});
