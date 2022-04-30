fdcacad211db37014620286fd8a4cea757f60e11
Veronika Sesay <veronika.sesay@gmail.com>
2022-04-30 11:45:48 +0200

    Schovávání menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.getElementById('nav-btn');
+const nav = document.querySelector('nav');
+navBtn.addEventListener('click', () => {
+  nav.classList.toggle('nav-closed');
+});
+
+const navAll = document.querySelectorAll('nav a');
+navAll.forEach((item) =>
+  item.addEventListener('click', () => {
+    nav.classList.add('nav-closed');
+  }),
+);
