83a94820899e75782e1e20b5efc4d9cfacb98bc8
Veronika Sesay <veronika.sesay@gmail.com>
2022-04-30 17:00:36 +0200

    Funkční tlačítko

--- src/Drink/index.js
+++ src/Drink/index.js
@@ -3,7 +3,8 @@ import './style.css';
 import { Layer } from '../Layer/index.js';
 
 export const Drink = (props) => {
-  const { id, name, ordered, layers, image } = props;
+  const { id, name, layers, image } = props;
+  let { ordered } = props;
   const drinkProduct = document.createElement('div');
   drinkProduct.classList.add('drink');
   drinkProduct.innerHTML = `
@@ -22,6 +23,19 @@ export const Drink = (props) => {
   const drinkInfo = drinkProduct.querySelector('.drink__info');
   layers.forEach((layer) => {
     drinkInfo.appendChild(Layer(layer));
+  });
+  const orderBtn = drinkProduct.querySelector('.order-btn');
+  const drinkCup = drinkProduct.querySelector('.drink__cup');
+  orderBtn.addEventListener('click', () => {
+    if (ordered === false) {
+      orderBtn.textContent = 'Zrušit';
+      drinkCup.classList.add('drink__cup--selected');
+      ordered = true;
+    } else {
+      orderBtn.textContent = 'Objednat';
+      drinkCup.classList.remove('drink__cup--selected');
+      ordered = false;
+    }
   });
   return drinkProduct;
 };
--- src/index.js
+++ src/index.js
@@ -17,24 +17,6 @@ navAll.forEach((item) =>
   }),
 );
 
-//objednávání kávy
-
-const orderBtn = document.querySelector('.order-btn');
-const drinkCup = document.querySelector('.drink__cup');
-
-/*let ordered = false;
-orderBtn.addEventListener('click', () => {
-  if (ordered === false) {
-    orderBtn.textContent = 'Zrušit';
-    drinkCup.classList.add('drink__cup--selected');
-    ordered = true;
-  } else {
-    orderBtn.textContent = 'Objednat';
-    drinkCup.classList.remove('drink__cup--selected');
-    ordered = false;
-  }
-});*/
-
 //ingredience jako komponenty
 
 import { Drink } from './Drink/index.js';
